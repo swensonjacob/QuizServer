b96ffc489886ff608d9526797d3d1ef146052700
EliasMehr <elias.mehr@gmail.com>
2019-11-12 11:31:32 +0100

    added files to package

--- src/quiz/ClientHandler.java
+++ src/quiz/ClientHandler.java
@@ -1,3 +1,4 @@
+package quiz;
 
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
--- src/quiz/QuizServer.java
+++ src/quiz/QuizServer.java
@@ -1,3 +1,5 @@
+package quiz;
+
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
